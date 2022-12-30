9ef795b05ffd9e8dc5fca8d8a06eae9c4e190c33
TheOnlyFastCoder2 <ponomareow.alex2015@yandex.ru>
2022-12-30 20:10:09 +0400

    update

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -7,19 +7,36 @@ import PLogo from 'public/imgs/logo.png';
 export default function () {
 
   const [mode, setMode] = useState<boolean>(false);
+  const [indButton, setIndButton] = useState<number>(0);
 
   function toggleMenu () {
     setMode(mode ? false : true);
   }
 
-
+  function setButton(ind: number) {
+    setIndButton(ind);
+  }
 
   return (
     <div className={`${st.Nav}`}>
       <img src={PLogo.src}/>
         <ul className={`${st.menu} ${mode ? st.active : "" }`}>
-        <li><Link href={'/'}>главная</Link></li>
-        <li><Link href={'/About'}>О себе</Link></li>
+        {
+          [
+            {name:"главная", link: '/'},
+            {name:"О себе", link: '/About'}
+
+          ].map ( ({name,link}, ind) => {
+            return (
+              <li 
+                key={ind}
+        
+                onClick={() => setButton(ind)}>
+                <Link href={link}>{name}</Link>
+              </li>
+            )
+          }) 
+        }
       </ul>
       <button onClick={toggleMenu}><hr/><hr/><hr/></button>
     </div>
